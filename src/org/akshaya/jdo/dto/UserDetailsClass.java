93043547ab8d3f6efabe376f4e746d26e27a4e65
AkshayaT26 <shiny.akshai@gmail.com>
2017-08-17 18:36:39 -0700

    named queries

--- src/org/akshaya/jdo/dto/UserDetailsClass.java
+++ src/org/akshaya/jdo/dto/UserDetailsClass.java
@@ -5,12 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.NamedQuery;
 
 
 
 //Data Model Class - Entity class
 
 @Entity
+@NamedQuery(name="UserDetailsClass.byId", query = "from UserDetailsClass where userId = ?")
+@NamedNativeQuery(name="UserDetailsClass.byName", query ="select * from UserDetailsClass where userName =?", resultClass=UserDetailsClass.class)
 public class UserDetailsClass {
 	
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
--- src/org/akshaya/jdo/hibernate/HibernateTest.java
+++ src/org/akshaya/jdo/hibernate/HibernateTest.java
@@ -1,6 +1,6 @@
 package org.akshaya.jdo.hibernate;
 
-
+import java.util.List;
 
 import org.akshaya.jdo.dto.UserDetailsClass;
 
@@ -8,19 +8,29 @@ import org.akshaya.jdo.dto.UserDetailsClass;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class HibernateTest {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args){
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session =sessionFactory.openSession();
 		session.beginTransaction();
 		
-		UserDetailsClass user = (UserDetailsClass)session.get(UserDetailsClass.class,5);
-		user.setUserName("Updated name");
+		
+		Query query =session.getNamedQuery("UserDetailsClass.byName");
+		query.setString(0,"user10");
+		
+		
+		List<UserDetailsClass> users = (List<UserDetailsClass>) query.list();
 		session.getTransaction().commit();
 		session.close();
+		
+		for(UserDetailsClass u:users){
+			System.out.println(u.getUserName());
+		}
 		
 	}
 }
